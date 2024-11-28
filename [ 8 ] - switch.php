354cd693e198bb9fddc6d7000a11afb3f93b2034
Yousuf AL Hdhrami <izkiboy@gmail.com>
2024-11-28 08:48:07 +0300

    created the switch.php file

---
+++ [ 8 ] - switch.php
@@ -0,0 +1,17 @@
+<?php 
+
+$value = 20;
+
+switch($value){
+
+    case $value == 10 : echo "The number is 10";
+    break;
+
+    case $value == 20 : echo "The number is 20";
+    break;
+
+    case $value == 30 : echo "The number is 30";
+    break;
+
+    default : echo "Unknown Number";
+}
